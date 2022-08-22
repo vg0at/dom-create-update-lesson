bd1d496498d327b1ea7507dcf5b3fc54dc1405d4
Angie Munoz <angiepmunoz@gmail.com>
2022-08-22 11:52:13 -0400

    creating and updating the dom

--- app.js
+++ app.js
@@ -69,3 +69,68 @@ const aside = document.querySelector('aside li')
 
 
 const group = document.querySelectorAll('.group')
+
+//created li element 
+const bagsProductLi = document.createElement("li");
+bagsProductLi.textContent = "Bags"
+bagsProductLi.setAttribute("class", "product")
+bagsProductLi.textContent += " & Wallets"
+
+//grabbed the parent element to our new li
+const orderedList = document.querySelector("ol")
+
+//after- places element after the element calling the method
+//before - places element before the el calling the method 
+//prepend - adds el as first child to parent el
+//append - adds el as the last child to parent el 
+
+// added the bags li to the bottom of our ol
+// when we use append and prepend the element is added as a child
+orderedList.append(bagsProductLi);
+
+//when we use before and after the element is added as a sibling to the parent
+// orderedList.before(bagsProductLi)
+
+// create header
+// child of div (.container)
+// need to create h1 - child of header
+// create nav el
+// create nav - child of header 
+// create the ul
+// nav is parent to the ul
+// create li as child of ul
+// create a tag - child of li 
+// create class (attribute)
+
+const header = document.createElement("header");
+// const divContainer = document.querySelector("div.container")
+container.prepend(header)
+
+const headerH1 = document.createElement("h1");
+header.append(headerH1)
+headerH1.textContent = "DOM MANIPULATION"
+
+const nav = document.createElement("nav")
+// console.log(nav)
+// header.append(nav)
+headerH1.after(nav)
+
+const ul = document.createElement("ul");
+nav.append(ul)
+
+const navValues = ["Home", "About", "Contact", "Reviews"]
+
+
+for(let i = 0; i < 4; i++){
+    const li = document.createElement("li")
+   ul.append(li)
+   const anchor = document.createElement("a")
+   anchor.setAttribute("href", "#")
+
+   li.append(anchor)
+   anchor.textContent = navValues[i]
+}
+
+bagsProductLi.classList.add("group");
+// bagsProductLi.classList.remove("group");
+// bagsProductLi.classList.toggle("group")
